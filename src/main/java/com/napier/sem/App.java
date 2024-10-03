975df5488e63cb3f277d6c2e0afef2835337681a
ShineHponeMyat-Honours-Project <40685571@live.napier.ac.uk>
2024-10-04 00:50:11 +0700

    Issued 12 to 16

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -276,6 +276,79 @@ public class App
     }
 
     /**
+     * Retrieves the top N populated cities in the world.
+     * @param number The number of cities to retrieve.
+     * @return SQL query string to get top N cities in the world.
+     */
+    public String topPopulatedCitiesInWorld(int number) {
+        return "SELECT city.Name, country.Name as Country, city.District, city.Population  "
+                + "FROM city "
+                + "JOIN country ON city.CountryCode = country.Code "
+                + "ORDER BY city.Population DESC "
+                + "LIMIT " + number;
+    }
+
+    /**
+     * Retrieves the top N populated cities in a continent.
+     * @param number The number of cities to retrieve.
+     * @param continent The continent to retrieve cities from.
+     * @return SQL query string to get top N cities in a continent.
+     */
+    public String topPopulatedCitiesInContinent(int number, String continent) {
+        return "SELECT city.Name, country.Name as Country, city.District, city.Population  "
+                + "FROM city "
+                + "JOIN country ON city.CountryCode = country.Code "
+                + "WHERE country.Continent = '" + continent + "' "
+                + "ORDER BY city.Population DESC "
+                + "LIMIT " + number;
+    }
+
+    /**
+     * Retrieves the top N populated cities in a region.
+     * @param number The number of cities to retrieve.
+     * @param region The region to retrieve cities from.
+     * @return SQL query string to get top N cities in a region.
+     */
+    public String topPopulatedCitiesInRegion(int number, String region) {
+        return "SELECT city.Name, country.Name as Country, city.District, city.Population  "
+                + "FROM city "
+                + "JOIN country ON city.CountryCode = country.Code "
+                + "WHERE country.Region = '" + region + "' "
+                + "ORDER BY city.Population DESC "
+                + "LIMIT " + number;
+    }
+
+    /**
+     * Retrieves the top N populated cities in a country.
+     * @param number The number of cities to retrieve.
+     * @param country The country to retrieve cities from.
+     * @return SQL query string to get top N cities in a country.
+     */
+    public String topPopulatedCitiesInCountry(int number, String country) {
+        return "SELECT city.Name, country.Name as Country, city.District, city.Population  "
+                + "FROM city "
+                + "JOIN country ON city.CountryCode = country.Code "
+                + "WHERE country.Name = '" + country + "' "
+                + "ORDER BY city.Population DESC "
+                + "LIMIT " + number;
+    }
+
+    /**
+     * Retrieves the top N populated cities in a district.
+     * @param number The number of cities to retrieve.
+     * @param district The district to retrieve cities from.
+     * @return SQL query string to get top N cities in a district.
+     */
+    public String topPopulatedCitiesInDistrict(int number, String district) {
+        return "SELECT city.Name, country.Name as Country, city.District, city.Population  "
+                + "FROM city "
+                + "JOIN country ON city.CountryCode = country.Code "
+                + "WHERE city.District = '" + district + "' "
+                + "ORDER BY city.Population DESC "
+                + "LIMIT " + number;
+    }
+
+    /**
      * get the cities information as an arraylist
      * @param query the SQL query to execute
      */
@@ -423,6 +496,40 @@ public class App
         a.printCities(citiesInDistrict);
         System.out.println(line);
 
+        // top N populated cities in the world
+        ArrayList<City> topCitiesInWorld = a.getAllCities(a.topPopulatedCitiesInWorld(5)); // Change the number as needed
+        System.out.println("Top N populated cities in the world");
+        System.out.println(titleLine);
+        a.printCities(topCitiesInWorld);
+        System.out.println(line);
+
+// top N populated cities in a specific continent (e.g., Asia)
+        ArrayList<City> topCitiesInContinent = a.getAllCities(a.topPopulatedCitiesInContinent(5, "Asia")); // Change the number as needed
+        System.out.println("Top N populated cities in the continent (Asia)");
+        System.out.println(titleLine);
+        a.printCities(topCitiesInContinent);
+        System.out.println(line);
+
+//top N populated cities in a specific region (e.g., Southeast Asia)
+        ArrayList<City> topCitiesInRegion = a.getAllCities(a.topPopulatedCitiesInRegion(5, "Southeast Asia")); // Change the number as needed
+        System.out.println("Top N populated cities in the region (Southeast Asia)");
+        System.out.println(titleLine);
+        a.printCities(topCitiesInRegion);
+        System.out.println(line);
+
+// top N populated cities in a specific country (e.g., "United States")
+        ArrayList<City> topPopulatedCitiesInCountry = a.getAllCities(a.topPopulatedCitiesInCountry(5, "United States")); // Change the number as needed
+        System.out.println("Top N populated cities in the country (United States)");
+        System.out.println(titleLine);
+        a.printCities(topPopulatedCitiesInCountry);
+        System.out.println(line);
+
+// top N populated cities in a specific district (e.g., "Buenos Aires")
+        ArrayList<City> topCitiesInDistrict = a.getAllCities(a.topPopulatedCitiesInDistrict(5, "Buenos Aires")); // Change the number as needed
+        System.out.println("Top N populated cities in the district (Buenos Aires)");
+        System.out.println(titleLine);
+        a.printCities(topCitiesInDistrict);
+        System.out.println(line);
 
         // Disconnect from database
         a.disconnect();
